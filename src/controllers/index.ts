e405f52ec97f9499cd74528672105ada1eed6824
Alexandru Constantin <alexandru.constantin@brokergenius.com>
2024-03-08 00:44:03 +0100

    Changes

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -6,102 +6,121 @@ import { BmpEncoder } from '../utils/bmpEncoder';
 
 // Format: [RGBA, RGBA ...]
 const customPaletteRedBlackWhite = [
-  255, 0, 0, 0, 
+  255, 0, 0, 0,
   0, 0, 0, 0,
   255, 255, 255, 0
 ];
 
+const customPaletteRedYellowBlueGreenBlackWhite = [
+  255, 0, 0, 0,
+  255, 250, 205, 0,
+  0, 0, 255, 0,
+  0, 255, 0, 0,
+  0, 0, 0, 0,
+  255, 255, 255, 0
+];
+
+const customPalettes = [
+  customPaletteRedBlackWhite,
+  customPaletteRedYellowBlueGreenBlackWhite
+]
+
 class IndexController {
-  async getIndex(req: Request, res: Response) {
-    const browser = await puppeteer.launch({
-      headless: true,
-      product: 'chrome',
-    });
-    const width = req.query.width ? parseInt(req.query.width as string) : 800;
-    const height = req.query.height ? parseInt(req.query.height as string) : 480;
-    const url = req.query.url ? req.query.url as string : `https://picsum.photos/${width}/${height}`;
-    const page = await browser.newPage();
-    await page.setViewport({ width, height });
+  async getIndex(req: Request, res: Response, next: any) {
+    try {
+      const browser = await puppeteer.launch({
+        headless: true,
+        product: 'chrome',
+      });
+      const width = req.query.width ? parseInt(req.query.width as string) : 800;
+      const height = req.query.height ? parseInt(req.query.height as string) : 480;
+      const paletteIndex = req.query.palette ? parseInt(req.query.palette as string) : 0;
+      const customPalette = customPalettes[paletteIndex];
+      const url = req.query.url ? req.query.url as string : `https://picsum.photos/${width}/${height}`;
+      const page = await browser.newPage();
+      await page.setViewport({ width, height });
 
-    await page.goto(url);
+      await page.goto(url);
 
-    const screenshot = await page.screenshot();
+      const screenshot = await page.screenshot();
 
-    await browser.close();
+      await browser.close();
 
-    const meta = PNG.sync.read(screenshot);
+      const meta = PNG.sync.read(screenshot);
 
-    const inPointContainer = utils.PointContainer.fromUint8Array(
-      meta.data,
-      meta.width,
-      meta.height,
-    );
+      const inPointContainer = utils.PointContainer.fromUint8Array(
+        meta.data,
+        meta.width,
+        meta.height,
+      );
 
-    const customPoints = utils.PointContainer.fromUint8Array(
-      customPaletteRedBlackWhite,
-      customPaletteRedBlackWhite.length / 4,
-      1,
-    );
-    const colorDistanceFormula = 'euclidean';
-    // 'cie94-textiles''cie94-graphic-arts''ciede2000''color-metric'
-    // 'euclidean' 'euclidean-bt709-noalpha'
-    // 'euclidean-bt709' 'manhattan' 'pngquant
+      const customPoints = utils.PointContainer.fromUint8Array(
+        customPalette,
+        customPalette.length / 4,
+        1,
+      );
+      const colorDistanceFormula = 'euclidean';
+      // 'cie94-textiles''cie94-graphic-arts''ciede2000''color-metric'
+      // 'euclidean' 'euclidean-bt709-noalpha'
+      // 'euclidean-bt709' 'manhattan' 'pngquant
 
-    const imageQuantization = 'stucki';
-    // 'nearest' 'riemersma''floyd-steinberg''false-floyd-steinberg''stucki'
-    // 'atkinson''jarvis' 'burkes' 'sierra' 'two-sierra' 'sierra-lite'
-    //  stucki / jarvis / sierra
+      const imageQuantization = 'stucki';
+      // 'nearest' 'riemersma''floyd-steinberg''false-floyd-steinberg''stucki'
+      // 'atkinson''jarvis' 'burkes' 'sierra' 'two-sierra' 'sierra-lite'
+      //  stucki / jarvis / sierra
 
-    const paletteQuantization = 'rgbquant'
-    // 'neuquant' 'neuquant-float' 'rgbquant' 'wuquant'
+      const paletteQuantization = 'rgbquant'
+      // 'neuquant' 'neuquant-float' 'rgbquant' 'wuquant'
 
-    const palette = buildPaletteSync([customPoints], {
-      colorDistanceFormula,
-      paletteQuantization,
-      colors: customPaletteRedBlackWhite.length / 4,
-    });
+      const imageQPalette = buildPaletteSync([customPoints], {
+        colorDistanceFormula,
+        paletteQuantization,
+        colors: customPalette.length / 4,
+      });
 
-    const outPointContainer = applyPaletteSync(inPointContainer, palette, {
-      colorDistanceFormula,
-      imageQuantization,
-    });
+      const outPointContainer = applyPaletteSync(inPointContainer, imageQPalette, {
+        colorDistanceFormula,
+        imageQuantization,
+      });
 
-    const outArrayRGBA = outPointContainer.toUint8Array();
+      const outArrayRGBA = outPointContainer.toUint8Array();
 
-    const outArrayABGR = new Uint8Array(outArrayRGBA.length);
+      const outArrayABGR = new Uint8Array(outArrayRGBA.length);
 
-    const bmpPalette: { red: number; green: number; blue: number; quad: number; }[] = [];
-    for (let i = 0; i < outArrayRGBA.length; i += 4) {
-      const color = {
-        red: outArrayRGBA[i],
-        green: outArrayRGBA[i + 1],
-        blue: outArrayRGBA[i + 2],
-        quad: outArrayRGBA[i + 3],
+      const bmpPalette: { red: number; green: number; blue: number; quad: number; }[] = [];
+      for (let i = 0; i < outArrayRGBA.length; i += 4) {
+        const color = {
+          red: outArrayRGBA[i],
+          green: outArrayRGBA[i + 1],
+          blue: outArrayRGBA[i + 2],
+          quad: outArrayRGBA[i + 3],
+        }
+        const hasColor = bmpPalette.some((c) => c.red === color.red && c.green === color.green && c.blue === color.blue);
+        if (!hasColor) {
+          bmpPalette.push(color);
+        }
+        outArrayABGR[i] = color.quad
+        outArrayABGR[i + 1] = color.blue
+        outArrayABGR[i + 2] = color.green
+        outArrayABGR[i + 3] = color.red;
       }
-      const hasColor = bmpPalette.some((c) => c.red === color.red && c.green === color.green && c.blue === color.blue);
-      if (!hasColor) {
-        bmpPalette.push(color);
-      }
-      outArrayABGR[i] = color.quad // Alpha
-      outArrayABGR[i + 1] = color.blue // Blue
-      outArrayABGR[i + 2] = color.green // Green
-      outArrayABGR[i + 3] = color.red; // Red
 
+      const newBMP = new BmpEncoder({
+        data: outArrayABGR,
+        width: outPointContainer.getWidth(),
+        height: outPointContainer.getHeight(),
+        bitPP: 8,
+        colors: bmpPalette.length,
+        palette: bmpPalette
+      });
+
+      newBMP.encode();
+
+      res.setHeader('Content-Type', 'image/bmp');
+      res.send(newBMP.data);
+    } catch (error) {
+      next(error);
     }
-
-    const newBMP = new BmpEncoder({
-      data: outArrayABGR,
-      width: outPointContainer.getWidth(),
-      height: outPointContainer.getHeight(),
-      bitPP: 8,
-      colors: bmpPalette.length,
-      palette: bmpPalette
-    });
-
-    newBMP.encode();
-
-    res.setHeader('Content-Type', 'image/bmp');
-    res.send(newBMP.data);
   }
 }
 
